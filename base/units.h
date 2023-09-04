45c9a91b13cbb7251d171a5009168b5f7f1d8f6f
Miriam Polzer <miriam.polzer@google.com>
2023-09-04 06:37:20 +0200

    unit for distance, speed and movement

--- base/units.h
+++ base/units.h
@@ -3,7 +3,15 @@
 
 #include <bits/stdc++.h>
 
-enum class UnitType { Time, Adrenaline, Health, Energy, Percent };
+enum class UnitType {
+  Time,
+  Adrenaline,
+  Health,
+  Energy,
+  Percent,
+  Inches,
+  Speed
+};
 
 template <UnitType type, class ValueType>
 class Unit {
@@ -89,6 +97,7 @@ using Adrenaline = Unit<UnitType::Adrenaline, int>;
 
 const Adrenaline Strike = Adrenaline(25);
 
+// TODO unused so far.
 using Health = Unit<UnitType::Health, double>;
 using Energy = Unit<UnitType::Energy, double>;
 
@@ -99,5 +108,45 @@ T of(T value, Percent percent) {
   // assert(percent.value() >= 0 && percent.value() <= 100);
   return value * percent.value() / 100.0;
 }
+
+using Inches = Unit<UnitType::Inches, double>;
+
+const Inches AdjacentRange = Inches(144);
+const Inches EarshotRange = Inches(1000);
+
+const Inches CastingRange = Inches(1248);
+
+const Inches ShortbowRange = Inches(1004);
+const Inches SpearRange = ShortbowRange;
+
+const Inches HornbowRange = Inches(1273);
+const Inches RecurvebowRange = HornbowRange;
+
+const Inches FlatbowRange = Inches(1498);
+const Inches LongbowRange = FlatbowRange;
+
+// Passive spirit range.
+const Inches SpiritRange = Inches(2512);
+
+const Inches PartyRange = Inches(5020);
+
+using Position = std::pair<Inches, Inches>;
+
+Inches Distance(const Position& p1, const Position& p2);
+
+bool InRange(const Position& p1, const Position& p2, const Inches& range);
+
+// Speed is measured in Inches per millisecond
+using Speed = Unit<UnitType::Speed, Inches>;
+
+const Speed WalkingSpeed = Speed(Inches(0.250));
+
+using Direction = std::pair<Inches, Inches>;
+
+Direction Towards(const Position& from, const Position& to);
+
+// Calculates the position after one tick if moving with speed in direction.
+Position NextPosition(const Position& current, const Direction& direction,
+                      const Speed& speed);
 
 #endif  // BASE_UNITS_H
--- base/units_test.cpp
+++ base/units_test.cpp
@@ -3,6 +3,8 @@
 #include <bits/stdc++.h>
 #include <gtest/gtest.h>
 
+#include "logging.h"
+
 namespace {
 const double epsilon = 1.0e-10;
 }
@@ -28,4 +30,63 @@ TEST(UnitsTest, PercentOfInteger) { ASSERT_EQ(of(10, Percent(10)), 1); }
 
 TEST(UnitsTest, PercentOfDouble) {
   ASSERT_TRUE(std::abs(of(30.0, Percent(100.0 / 3.0)) - 10.0) < epsilon);
+}
+
+TEST(UnitsTest, DistanceLine) {
+  Position p1 = {Inches(0), Inches(10)};
+  Position p2 = {Inches(0), Inches(-10)};
+  ASSERT_EQ(Distance(p1, p2), Inches(20));
+}
+
+TEST(UnitsTest, DistanceComplex) {
+  Position p1 = {Inches(5), Inches(6)};
+  Position p2 = {Inches(3), Inches(7)};
+  ASSERT_TRUE(Distance(p1, p2) - Inches(2.23607) < Inches(0.0001));
+}
+
+TEST(UnitsTest, InRange) {
+  Position p1 = {Inches(5), Inches(3)};
+  Position p2 = {Inches(3), Inches(7)};
+  ASSERT_TRUE(InRange(p1, p2, Inches(4.5)));
+  ASSERT_FALSE(InRange(p1, p2, Inches(4.3)));
+}
+
+TEST(UnitsTest, MoveHorizontally) {
+  Position p = {Inches(1), Inches(2)};
+  Direction d = {Inches(1), Inches(0)};
+  Speed s(Inches(1));
+  Position next = {Inches(2), Inches(2)};
+  ASSERT_EQ(NextPosition(p, d, s), next);
+}
+
+TEST(UnitsTest, MoveVertically) {
+  Position p = {Inches(1), Inches(2)};
+  Direction d = {Inches(0), Inches(1)};
+  Speed s(Inches(1));
+  Position next = {Inches(1), Inches(3)};
+  ASSERT_EQ(NextPosition(p, d, s), next);
+}
+
+TEST(UnitsTest, MovingOutOfAggroCircleTakes4SecondsDiagonally) {
+  Position p = {Inches(0), Inches(0)};
+  Position initial = p;
+  const Direction d = {Inches(1), Inches(1)};
+  const Speed s = WalkingSpeed;
+  for (int i = 0; Time(i) <= 4 * Second; ++i) {
+    ASSERT_TRUE(InRange(initial, p, EarshotRange));
+    p = NextPosition(p, d, s);
+  }
+  ASSERT_FALSE(InRange(initial, p, EarshotRange));
+}
+
+TEST(UnitsTest, MovingOutOfAggroCircleTakes4SecondsWithRandomDirection) {
+  Position p = {Inches(0), Inches(0)};
+  Position initial = p;
+  const Direction d = {Inches(153), Inches(185)};
+  const Speed s = WalkingSpeed;
+  for (int i = 0; Time(i) <= 4 * Second; ++i) {
+    ASSERT_TRUE(InRange(initial, p, EarshotRange));
+    p = NextPosition(p, d, s);
+  }
+  ASSERT_FALSE(InRange(initial, p, EarshotRange));
 }
